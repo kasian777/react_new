bfe7ea3e6cdc2fc1a1eadb535abe011565ce7c14
kasian777 <kasian.dima@ukr.net>
2022-02-05 16:54:07 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {useReducer} from "react";
 
 const reduser = (state, action)=>{
+    state={...state, count2:state.count2+1}
+state={...state, count3:state.count2*2}
 switch (action.tupe){
     case 'inc':
         return {...state, count1:state.count1+1}
@@ -8,22 +10,26 @@ switch (action.tupe){
         return {...state, count1:state.count1-1}
     case 'reset':
         return {...state, count1: action.payload}
+    defolt:
+    throw new Error('ERROR')
 }
     return state
 }
 
 
 function App() {
-    const [state, dispatch] = useReducer(reduser,{count1:0 })
+    const [state, dispatch] = useReducer(reduser,{count1:0, count2:0, count3:0 })
 
       
 
   return (
     <div>
        <div> {state.count1}</div>
+        <div> {state.count2}</div>
+        <div> {state.count3}</div>
         <button onClick={()=>dispatch({tupe:'inc'})}>Inc</button>
         <button onClick={()=>dispatch({tupe:'dec'})}>Dec</button>
-        <button onClick={()=>dispatch({tupe:'reset', payload:8})}>Reset</button>
+        <button onClick={()=>dispatch({tupe:'reset', payload:0})}>Reset</button>
     </div>
   );
 }
